216fdfb4484ba5bc322a32f89cea480b39f326c4
RBELLI <bellirodrigo2@gmail.com>
2024-07-10 17:55:27 -0300

    ...

--- tests/test_base_asset.py
+++ tests/test_base_asset.py
@@ -4,7 +4,12 @@ from unittest.mock import MagicMock
 
 import pytest
 
-from am.asset import ParentChildAsset, TargetAsset
+from am.asset import (
+    InconsistentIdTypeError,
+    ObjHierarchyError,
+    ParentChildAsset,
+    TargetAsset,
+)
 from am.container import Factory
 from am.interfaces import Repository
 from am.schemas.webid import WebId
@@ -49,7 +54,7 @@ def test_target_asset_ok(target: str, webid: WebId, repo: Repository):
 )
 def test_target_asset_nok(target: str, webid: WebId, repo: Repository):
 
-    with pytest.raises(expected_exception=Exception):
+    with pytest.raises(expected_exception=InconsistentIdTypeError):
         TargetAsset(_repo=repo, _factory=factory, _target=target, _webid=webid)
 
 
@@ -66,3 +71,20 @@ def test_parent_asset_ok(target: str, webid: WebId, repo: Repository, child: str
     ParentChildAsset(
         _repo=repo, _factory=factory, _target=target, _webid=webid, _child=child
     )
+
+
+@pytest.mark.parametrize(
+    "target, webid, child",
+    [
+        ("BaseServer", WebId.make(input="serv668540fb5ac420d8fc35320a"), "BaseItem"),
+        ("BaseRoot", WebId.make(input="root668540fb5ac420d8fc35320a"), "BaseServer"),
+        ("BaseNode", WebId.make(input="node668540fb5ac420d8fc35320a"), "BaseRoot"),
+        ("BaseItem", WebId.make(input="item668540fb5ac420d8fc35320a"), "BaseNode"),
+    ],
+)
+def test_parent_asset_nok(target: str, webid: WebId, repo: Repository, child: str):
+
+    with pytest.raises(expected_exception=ObjHierarchyError):
+        ParentChildAsset(
+            _repo=repo, _factory=factory, _target=target, _webid=webid, _child=child
+        )
